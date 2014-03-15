d769dcc2a3834d88aa300dd4c44cfe372e6d9d59
Michael Zaporozhets <michaelzaporozhets@gmail.com>
2014-03-15 22:07:48 +1100

    lol

--- hishtory.js/scripts/main.js
+++ hishtory.js/scripts/main.js
@@ -29,7 +29,7 @@ var openPage = function(page) {
     $('section.page#'+page).addClass('current');
 };
 
-SH.info.root = 'simpler-history/';
+SH.info.root = 'hishtory.js/';
 SH.init(openPage,{forceFallback:false});
 
 jQuery(function ($) {
